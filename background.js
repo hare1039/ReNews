e5ea3240d28dc2b9e7c77446ba5a292680a6fb87
hare1039 <hare39393@gmail.com>
2021-02-01 10:25:17 -0500

    Add current tab reload

--- background.js
+++ background.js
@@ -2,8 +2,6 @@ var defaultSettings = {
     newsHosts: ["nytimes.com", "theguardian.com", "sciencemag.org", "nature.com"]
 };
 
-function onError(e) { console.error(e); }
-
 function checkStoredSettings(storedSettings) {
     if (!storedSettings.since || !storedSettings.newsHosts) {
         browser.storage.local.set(defaultSettings);
@@ -11,11 +9,18 @@ function checkStoredSettings(storedSettings) {
 }
 
 const gettingStoredSettings = browser.storage.local.get();
-gettingStoredSettings.then(checkStoredSettings, onError);
+gettingStoredSettings.then(checkStoredSettings, console.error);
 
 function forget(storedSettings) {
     const newsHosts = storedSettings.newsHosts;
     console.log(newsHosts);
+
+    function reload(tabs) {
+        let tab = tabs[0];
+        console.log(tab.url);
+        let reloading = browser.tabs.reload(tab.id);
+        reloading.then(() => {console.log("loaded");}, console.error);
+    };
 
     function notify() {
         let newsHostsString = newsHosts.join(", ");
@@ -24,17 +29,19 @@ function forget(storedSettings) {
             "title": "Removed browsing data",
             "message": `Removed ${newsHostsString} cookies`
         });
-    }
+
+        browser.tabs.query({currentWindow: true, active: true}).then(reload, console.error);
+    };
 
     browser.browsingData.remove({
         hostnames: newsHosts,
         since: 0
     }, {
         cookies: true
-    }).then(notify, onError);
+    }).then(notify, console.error);
 }
 
 browser.browserAction.onClicked.addListener(() => {
     const gettingStoredSettings = browser.storage.local.get();
-    gettingStoredSettings.then(forget, onError);
+    gettingStoredSettings.then(forget, console.error);
 });
